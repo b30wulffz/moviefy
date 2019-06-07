008a407a4286f5c9507928e1fa8d1d6358265653
b30wulffz <shlokpandey123@gmail.com>
2019-06-07 16:44:06 +0530

    Updates

---
+++ precache-manifest.421ff4d6cd0dc525a91c592e7eb1b744.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a3a831f01dd0bebb8e3982747e0713",
+    "url": "/moviefy/index.html"
+  },
+  {
+    "revision": "55847c69499317b79cf9",
+    "url": "/moviefy/static/css/main.ba848e94.chunk.css"
+  },
+  {
+    "revision": "01a20e865de92390fbf0",
+    "url": "/moviefy/static/js/2.91ebf26e.chunk.js"
+  },
+  {
+    "revision": "55847c69499317b79cf9",
+    "url": "/moviefy/static/js/main.365db135.chunk.js"
+  },
+  {
+    "revision": "dde229af3155959f9868",
+    "url": "/moviefy/static/js/runtime~main.78c58e37.js"
+  }
+]);
