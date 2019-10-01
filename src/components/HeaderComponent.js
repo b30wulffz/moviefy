a09fe811dcf1be96e563eea8668f6e177fb60917
Guillermo Villalta <guillermovillalta@Guillermos-iMac.local>
2019-09-30 18:24:16 -0600

    navbar elements are no longer a list but links + function for dropdown functionality

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 function Header(){
+    const navbar = nav => {
+        var x = document.getElementById("myTopnav");
+
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
+
     return (
+
         <header>
             <div id="navbar">
                 <div id="nav-banner">MovieFy</div>
                 <div id="nav-button">
-                    <ul>
-                        <li>
-                            <NavLink to="/home" className="nav-link">Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/add_movie" className="nav-link">Add movie</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/login" className="nav-link">Login</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/signup" className="nav-link">Sign Up</NavLink>
-                        </li>
-                    </ul>
+                <div class="topnav" id="myTopnav">
+                    <NavLink to="/home" className="nav-link">Home</NavLink>
+                    <NavLink to="/add_movie" className="nav-link">Add movie</NavLink>
+                    <NavLink to="/login" className="nav-link">Login</NavLink>
+                    <NavLink to="/signup" className="nav-link">Sign Up</NavLink>
+                    <NavLink to="javascript" className="icon" onClick={navbar}>
+                    <i class="fa fa-bars"></i>  
+                    </NavLink>
+                </div>
                 </div>
             </div>
         </header>
